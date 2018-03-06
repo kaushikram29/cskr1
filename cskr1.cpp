8578e1ef442d10f27da7d5c185930929987b456c
kaushikram29 <37103207+kaushikram29@users.noreply.github.com>
2018-03-06 14:43:21 +0530

    Create cskr1.cpp

---
+++ cskr1.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+cout<<"enter the value:";
+cin>>a;
+if(a==0)
+{
+cout<<"given value is zero";
+}
+else if(a<0)
+{
+cout<<"given value is negative";
+}
+else
+{
+cout<<"given value is positive";
+}
+getch();
+}
